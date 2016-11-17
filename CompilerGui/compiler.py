a3c132d50754f89944a8522711beb3df6fe9afeb
Nathan Lowe <techwiz96@gmail.com>
2016-11-17 15:34:41 -0500

    Compiler Gui: Add Files and find compilers

--- CompilerGui/compiler.py
+++ CompilerGui/compiler.py
@@ -2,9 +2,12 @@
 
 from tkinter import *
 from tkinter.ttk import *
+from tkinter.filedialog import *
 from idlelib.WidgetRedirector import WidgetRedirector
+from subprocess import check_output
 
 
+COMPILERS = [{'text': "No Compilers Found", 'path': ""}]
 CPP_STANDARDS = ["c++14", "c++11", "c++99"]
 OPTIMIZE_FLAGS = [
     {'text': "-O0 (Optimize for Compile Time)", 'value': "-O0"},
@@ -12,6 +15,22 @@ OPTIMIZE_FLAGS = [
     {'text': "-O2 (Optimize for code size and speed, more than -O1)", 'value': "-O2"},
     {'text': "-O3 (Optimize for code size and speed, more than -O2)", 'value': "-O3"}
 ]
+
+
+def get_gcc_version(path):
+    return str(check_output([path, "--version"]).decode('UTF-8').split()[2])
+
+
+def find_compilers():
+    global COMPILERS
+
+    results = check_output(["whereis", "-b", "g++"])[5:-1].decode('UTF-8').split()
+    if len(results) == 0:
+        raise Exception("Could not find any installs of g++")
+
+    COMPILERS = []
+    for c in results:
+        COMPILERS.append({'text': "g++ " + get_gcc_version(c) + ": " + c, 'path': c})
 
 
 class ReadOnlyText(Text):
@@ -23,8 +42,10 @@ class ReadOnlyText(Text):
 
 
 class SourcesBox(Frame):
-    def __init__(self, master=None):
+    def __init__(self, master=None, callback=None):
         super().__init__(master)
+
+        self.callback_func = callback
 
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
@@ -36,18 +57,44 @@ class SourcesBox(Frame):
         self.sources_list = Listbox(self)
         self.sources_list.grid(row=0, column=0, columnspan=3, sticky=N+S+E+W)
 
-        self.clear_button = Button(self, text="Clear")
+        self.clear_button = Button(self, text="Clear", command=self.on_clear)
         self.clear_button.grid(row=1, column=0, sticky=N+S+E+W)
 
-        self.remove_button = Button(self, text="Remove")
+        self.remove_button = Button(self, text="Remove", command=self.on_remove)
         self.remove_button.grid(row=1, column=1, sticky=N+S+E+W)
 
-        self.add_button = Button(self, text="Add")
+        self.add_button = Button(self, text="Add", command=self.on_add)
         self.add_button.grid(row=1, column=2, sticky=N+S+E+W)
+
+    def on_clear(self, *args, **kwargs):
+        self.sources_list.delete(0, END)
+
+        self.callback_func()
+
+    def on_remove(self, *args, **kwargs):
+        self.sources_list.delete(ANCHOR)
+
+        self.callback_func()
+
+    def on_add(self, *args, **kwargs):
+        files = askopenfilenames(filetypes=[("C/C++ Files", ("*.cpp", "*.cxx", "*.cc", "*.c"))])
+
+        if not files:
+            return
+
+        prefix = os.getcwd()
+
+        for f in [os.path.relpath(p, prefix) for p in files]:
+            self.sources_list.insert(END, f)
+
+        self.callback_func()
+
+    def get_sources(self):
+        return self.sources_list.get(0, END)
 
 
 class MainWindow(Frame):
-    def __init__(self, compilers, master=None):
+    def __init__(self, master=None):
         super().__init__(master)
 
         self.__generated_cmd = ""
@@ -69,7 +116,7 @@ class MainWindow(Frame):
 
         self.compiler_select_label = Label(self, text="Compiler")
         self.compiler_select_label.grid(row=0, column=0, sticky=W+E)
-        self.compiler_select = Combobox(self, state=['readonly'], values=compilers)
+        self.compiler_select = Combobox(self, state=['readonly'], values=[x["text"] for x in COMPILERS])
         self.compiler_select.current(0)
         self.compiler_select.bind('<<ComboboxSelected>>', self.on_compiler_select)
         self.compiler_select.grid(row=0, column=1, sticky=W+E)
@@ -81,12 +128,13 @@ class MainWindow(Frame):
         self.standard_select.bind('<<ComboboxSelected>>', self.on_standard_select)
         self.standard_select.grid(row=0, column=3, columnspan=2, sticky=W+E)
 
-        self.sources = SourcesBox(self)
+        self.sources = SourcesBox(self, callback=self.rebuild_command)
         self.sources.grid(row=1, column=0, rowspan=3, columnspan=2, sticky=N+S+E+W)
 
         self.executable_label = Label(self, text="Executable")
         self.executable_label.grid(row=4, column=0, sticky=E+W)
         self.executable_data = StringVar()
+        self.executable_data.set("a.out")
         self.executable_data.trace("w", lambda *a, **kw: self.rebuild_command())
         self.executable = Entry(self, textvariable=self.executable_data)
         self.executable.grid(row=4, column=1, sticky=E+W)
@@ -103,7 +151,10 @@ class MainWindow(Frame):
         self.optimize_select.grid(row=1, column=3, sticky=E+W)
 
         self.generated_command = ReadOnlyText(self)
-        self.generated_command.grid(row=3, column=2, rowspan=2, columnspan=3, sticky=N+S+E+W)
+        self.generated_command.grid(row=3, column=2, columnspan=3, sticky=N+S+E+W)
+
+        self.compile_button = Button(self, text="Compile", command=self.run_compile)
+        self.compile_button.grid(row=4, column=2, columnspan=3, sticky=N+S+E+W)
 
         self.compiler_output = ReadOnlyText(self)
         self.compiler_output.grid(row=5, column=0, columnspan=5, sticky=N + E + S + W)
@@ -123,7 +174,7 @@ class MainWindow(Frame):
         self.rebuild_command()
 
     def rebuild_command(self):
-        self.__generated_cmd = self.compiler_select.get() + " "
+        self.__generated_cmd = COMPILERS[self.compiler_select.current()]["path"] + " "
         self.__generated_cmd += "-std=" + self.standard_select.get() + " "
         self.__generated_cmd += OPTIMIZE_FLAGS[self.optimize_select.current()]["value"] + " "
 
@@ -131,20 +182,30 @@ class MainWindow(Frame):
             self.__generated_cmd += "-g "
 
         if self.executable.get() is not None and self.executable.get() is not "":
-            self.__generated_cmd += "-O " + self.executable.get()
+            self.__generated_cmd += "-O " + self.executable.get() + " "
+
+        for file in self.sources.get_sources():
+            self.__generated_cmd += "'" + file + "' "
+
+        self.__generated_cmd.rstrip(' ')
 
         cmd = "Generated Command:\n\n" + self.__generated_cmd
 
         self.generated_command.delete("1.0", END)
         self.generated_command.insert(END, cmd)
 
+    def run_compile(self):
+        print(self.__generated_cmd)
+
 
 if __name__ == "__main__":
+    find_compilers()
+
     root = Tk()
     root.minsize(800, 480)
     root.rowconfigure(0, weight=1)
     root.columnconfigure(0, weight=1)
 
-    app = MainWindow(["/usr/bin/g++", "/usr/local/bin/g++"], master=root)
+    app = MainWindow(master=root)
     app.master.title("Compiler Helper")
     app.mainloop()
